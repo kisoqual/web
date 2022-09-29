ad83ccb333aa576afbf3c39db835608c8eb1464c
kisoqual <64285271+kisoqual@users.noreply.github.com>
2022-09-30 01:03:48 +0300

    Update and rename mmxxii_y.js to mmxxii.js

--- mmxxii.js
+++ mmxxii.js
@@ -1,10 +1,10 @@
 $('.r1').click(function(){
     if (!$(this).data('status')) {
-    $(this).html('<b><i>2010-2020-2⑨22</i></b>');
+    $(this).html('<b><i>2010-2020-2022</i></b>');
     $(this).data('status', true);
     }
     else {
-    $(this).html('<b><i>MMX-MMXX-MMCMXXII</i></b>');
+    $(this).html('<b><i>MMX-MMXX-MMXXII</i></b>');
     $(this).data('status', false);
     }
 });
