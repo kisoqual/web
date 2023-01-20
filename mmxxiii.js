40ed08c7247546b3fb9e3837752e04f5bdf431f5
kisoqual <64285271+kisoqual@users.noreply.github.com>
2023-01-21 02:16:42 +0300

    c

--- mmxxiii.js
+++ mmxxiii.js
@@ -1,10 +1,10 @@
 $('.r1').click(function(){
     if (!$(this).data('status')) {
-    $(this).html('<b><i>2010-2020-2023</i></b>');
+    $(this).html('<b><i>2010-2023</i></b>');
     $(this).data('status', true);
     }
     else {
-    $(this).html('<b><i>MMX-MMXX-MMXXIII</i></b>');
+    $(this).html('<b><i>MMX-MMXXIII</i></b>');
     $(this).data('status', false);
     }
 });
