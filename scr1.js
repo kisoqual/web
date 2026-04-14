1ebd8ca2b7abe728243d67df272d71ea4fabc702
kisoqual <sobacore@gmail.com>
2026-04-14 06:42:46 +0300

    some other stuff

--- scr1.js
+++ scr1.js
@@ -1,1 +1,16 @@
+// there's a script initiation
+/*
+(() => {
+    var script = document.createElement('script');
+    //script.src = "https://code.jquery.com/jquery-4.0.0.slim.min.js";
+    //script.src = "https://cdn.jsdelivr.net/npm/eruda@3.4.3/eruda.min.js";
+    document.body.append(script);
+    // that's for eruda
+    script.onload = () => {
+        eruda.init(); 
+    } 
+})();
+*/
+
+console.log("for adding script like jquery, you need insert script from scr1.js and paste n launch to console");
 console.log("https://so.bang.cl/neurocom");
