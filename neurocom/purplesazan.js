a08416513463f5d79b64da3cdb31c04eca399dbb
kisoqual <sobacore@gmail.com>
2026-05-04 09:11:11 +0300

    some changes

---
+++ neurocom/purplesazan.js
@@ -0,0 +1,12 @@
+function _exec(params) {
+    navigator.clipboard.writeText(params)
+    const dInf = document.createElement('dialog')
+    const clBtn = document.querySelector('.clsBtn')
+    dInf.className = 'dinf'
+    dInf.innerHTML = 'text is copied<br><button class=clsBtn onclick=document.querySelector(\'.dinf\').close()>close</button>'
+    document.body.append(dInf)
+    dInf.showModal()
+    //alert('text is copied')
+}
+// impossible to navigate
+//location.href = 'data:text/html;base64,aHR0cHM6Ly9kaXNrLnlhbmRleC5ydS9pL1lTeVN4TkZzNnFwRmpB'
