d27d8e8dbeb5414175d62d6dc895e1f1adfe9ba5
kisoqual <sobacore@gmail.com>
2026-05-04 09:23:16 +0300

    anoter changes

--- neurocom/purplesazan.js
+++ neurocom/purplesazan.js
@@ -1,12 +1,15 @@
-function _exec(params) {
-    navigator.clipboard.writeText(params)
+const aptBtn = document.querySelector('#accept')
+aptBtn.addEventListener('click', () => {
+    navigator.clipboard.writeText('data:text/html;' +
+        'base64,aHR0cHM6Ly9kaXNrLnlhbmRleC5ydS9pL1lTeVN4TkZzNnFwRmpB')
     const dInf = document.createElement('dialog')
     const clBtn = document.querySelector('.clsBtn')
     dInf.className = 'dinf'
-    dInf.innerHTML = 'text is copied<br><button class=clsBtn onclick=document.querySelector(\'.dinf\').close()>close</button>'
+    dInf.innerHTML = 'text is copied<br>' +
+        '<button class=clsBtn onclick=document.querySelector(\'.dinf\').close()>close</button>'
     document.body.append(dInf)
     dInf.showModal()
     //alert('text is copied')
-}
+})
 // impossible to navigate
 //location.href = 'data:text/html;base64,aHR0cHM6Ly9kaXNrLnlhbmRleC5ydS9pL1lTeVN4TkZzNnFwRmpB'
