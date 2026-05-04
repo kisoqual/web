6ed95ce8181cae79e494a6d9ba7eba868289e8c5
kisoqual <sobacore@gmail.com>
2026-05-04 09:26:47 +0300

    and another one

--- neurocom/purplesazan.js
+++ neurocom/purplesazan.js
@@ -1,12 +1,15 @@
 const aptBtn = document.querySelector('#accept')
 aptBtn.addEventListener('click', () => {
-    navigator.clipboard.writeText('data:text/html;' +
-        'base64,aHR0cHM6Ly9kaXNrLnlhbmRleC5ydS9pL1lTeVN4TkZzNnFwRmpB')
+    navigator.clipboard.writeText(`data:text/html;
+        base64,aHR0cHM6Ly9kaXNrLnlhbmRleC5ydS9pL1lTeVN4TkZzNnFwRmpB`)
     const dInf = document.createElement('dialog')
     const clBtn = document.querySelector('.clsBtn')
     dInf.className = 'dinf'
-    dInf.innerHTML = 'text is copied<br>' +
-        '<button class=clsBtn onclick=document.querySelector(\'.dinf\').close()>close</button>'
+    dInf.innerHTML = `text is copied<br>
+        <button class=clsBtn 
+            onclick=document.querySelector(\'.dinf\').close()>
+            close
+        </button>`
     document.body.append(dInf)
     dInf.showModal()
     //alert('text is copied')
