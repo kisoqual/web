d20f5f0001b0ded6ddbee30ba730f59ddcb8e22b
kisoqual <sobacore@gmail.com>
2026-05-14 07:21:49 +0300

    and another one

--- scr1.js
+++ scr1.js
@@ -25,3 +25,17 @@ txt1Ev.addEventListener('click', () => {
     document.body.append(flag)
     setTimeout(() => { flag.remove() }, 4096)
 })
+
+const bstyDiag = document.querySelector('.b00bsty-ex')
+bstyDiag.addEventListener('click', () => {
+const dInf = document.createElement('dialog')
+    const clBtn = document.querySelector('.clsBtn')
+    dInf.className = 'bdInf'
+    dInf.innerHTML = `<h2>no tiers available</h2>
+        <button class=clsBtn
+            onclick=document.querySelector(\'.bdInf\').close();window.location&nbsp;=&nbsp;'cw1QPh';>
+            close
+        </button>`
+    document.body.append(dInf)
+    dInf.showModal()
+})
