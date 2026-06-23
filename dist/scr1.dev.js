c995da360c50c0a5cce968bd078a0ba6a6534b42
kisoqual <sobacore@gmail.com>
2026-06-24 02:23:17 +0300

    refresh (β)eta

--- dist/scr1.dev.js
+++ dist/scr1.dev.js
@@ -16,22 +16,3 @@
 */
 console.log("for adding script like jquery, you need insert script from scr1.js and paste n launch to console");
 console.log("https://so.bang.cl/neurocom");
-var txt1Ev = document.querySelector('.txt1');
-txt1Ev.addEventListener('click', function () {
-  var flag = document.createElement('div');
-  flag.className = 'warnflag';
-  flag.innerHTML = "<span>\n            unavalible\n        </span>";
-  document.body.append(flag);
-  setTimeout(function () {
-    flag.remove();
-  }, 4096);
-});
-var bstyDiag = document.querySelector('.b00bsty-ex');
-bstyDiag.addEventListener('click', function () {
-  var dInf = document.createElement('dialog');
-  var clBtn = document.querySelector('.clsBtn');
-  dInf.className = 'bdInf';
-  dInf.innerHTML = "<h2>no tiers available</h2>\n        <button class=clsBtn\n            onclick=document.querySelector('.bdInf').close();window.location&nbsp;=&nbsp;'cw1QPh';>\n            close\n        </button>";
-  document.body.append(dInf);
-  dInf.showModal();
-});
--- scr1.js
+++ scr1.js
@@ -14,28 +14,3 @@
 
 console.log("for adding script like jquery, you need insert script from scr1.js and paste n launch to console");
 console.log("https://so.bang.cl/neurocom");
-
-const txt1Ev = document.querySelector('.txt1')
-txt1Ev.addEventListener('click', () => {
-    const flag = document.createElement('div')
-    flag.className = 'warnflag'
-    flag.innerHTML = `<span>
-            unavalible
-        </span>`
-    document.body.append(flag)
-    setTimeout(() => { flag.remove() }, 4096)
-})
-
-const bstyDiag = document.querySelector('.b00bsty-ex')
-bstyDiag.addEventListener('click', () => {
-const dInf = document.createElement('dialog')
-    const clBtn = document.querySelector('.clsBtn')
-    dInf.className = 'bdInf'
-    dInf.innerHTML = `<h2>no tiers available</h2>
-        <button class=clsBtn
-            onclick=document.querySelector(\'.bdInf\').close();window.location&nbsp;=&nbsp;'cw1QPh';>
-            close
-        </button>`
-    document.body.append(dInf)
-    dInf.showModal()
-})
